751231716c9f24359e8abcfa70852bb076a11558
BuildTools <unconfigured@null.spigotmc.org>
2024-12-23 22:39:21 -0300

    Sección 11

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrl: './basics-page.component.css'
+})
+export class BasicsPageComponent {
+
+  public nameLower: string = 'ignacio';
+  public nameUpper: string = 'IGNACIO';
+  public fullName: string = 'IgNAciO BiLBAo';
+
+}
